1249f35f4cfa15f556982021da26c53b8b75df09
LorenzoPacc <lorepaccio@gmail.com>
2026-01-18 23:00:01 +0100

    Auto backup 20260118_230001

--- autonomous_trading_bot.py
+++ autonomous_trading_bot.py
@@ -296,7 +296,7 @@ class AutonomousTradingBot:
 # Run bot
 if __name__ == "__main__":
     bot = AutonomousTradingBot(
-        initial_capital=200,
+        initial_capital=202.64,
         symbols=['BTC/USDT', 'ETH/USDT', 'SOL/USDT']
     )
     
